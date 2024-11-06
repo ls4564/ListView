5fed5d4b371e17cf6a7270e67428fd04e20c73bd
Lior1254 <liorshemtov2008@gmail.com>
2024-11-06 22:18:30 +0200

    the first page is finish

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.listview;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +15,76 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText eD1 , eD2;
+    Switch sw1;
+    Intent si;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        weddings();
+
+    }
+
+
+    public void weddings ()
+    {
+        /*
+        This function wedding all the widgets
+         */
+        eD1 = findViewById(R.id.eD1);
+        eD2 = findViewById(R.id.eD2);
+        sw1 = findViewById(R.id.sw1);
+
+    }
+
+    public void on_switch(View view)
+    {
+        if(sw1.isChecked())
+        {
+            eD2.setHint("Enter the difference");
+        }
+        else
+        {
+            eD2.setHint("Enter multiplication factor");
+        }
+    }
+
+    public void next_page(View view)
+    {
+        String str1, str2;
+        str1 = eD1.getText().toString();
+        if(str1.isEmpty())
+        {
+            str2 = eD2.getText().toString();
+            if(str2.isEmpty())
+            {
+                //code
+                si = new Intent(this, MainActivity2.class);
+
+                si.putExtra("a",Double.parseDouble(str1));//convert str -> double
+                si.putExtra("k",Double.parseDouble(str2));
+                si.putExtra("bool",sw1.isChecked());
+
+                startActivity(si);
+                //need to reset eD1 eD2
+
+
+
+
+            }
+            else
+            {
+                Toast.makeText(this, "You need Enter a difference or multiplication factor", Toast.LENGTH_SHORT);
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "You need Enter a number", Toast.LENGTH_SHORT);
+
+        }
     }
 }
